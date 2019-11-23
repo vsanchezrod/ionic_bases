4939f2ce6f391ad78f0e44c2c47dc24558cf53c1
Vir Rx Sanchez <v.sanchez.rod@gmail.com>
2019-11-23 21:06:34 +0100

    Ejemplo Avatar, dentro del ion-header, en los ion-items de un ion-list y dentro de los ion-chip

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -18,6 +18,11 @@ export class InicioPage implements OnInit {
       icon: 'appstore',
       name: 'Alert',
       redirectTo: '/alert'
+    },
+    {
+      icon: 'beaker',
+      name: 'Avatar',
+      redirectTo: '/avatar'
     }
   ];
 
