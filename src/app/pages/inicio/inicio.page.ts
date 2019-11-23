86706ee46e50accda7f52a2fd3bad5a9f8564055
Vir Rx Sanchez <v.sanchez.rod@gmail.com>
2019-11-23 18:31:43 +0100

    Creación de una ion-list

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  // Crear un array de Componentes
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
 }
