3611bed3c333ee1469de0bc0909a43827ec9cb09
Vir Rx Sanchez <v.sanchez.rod@gmail.com>
2020-02-01 12:07:04 +0100

    Nueva página con botones y navegación

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -23,6 +23,11 @@ export class InicioPage implements OnInit {
       icon: 'beaker',
       name: 'Avatar',
       redirectTo: '/avatar'
+    },
+    {
+      icon: 'radio-button-on',
+      name: 'Botones y router',
+      redirectTo: '/botones'
     }
   ];
 
